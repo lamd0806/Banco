8e0e03a31ae2adf57b0e2d6165e9bb4c937accba
lamd0806 <81428230+lamd0806@users.noreply.github.com>
2021-12-16 09:35:56 -0500

    Delete Banistmo directory

--- Banistmo/src/main/java/co/com/andrea/bancolombia/userinterfaces/InicioBancolombia.java
+++
@@ -1,7 +0,0 @@
-package co.com.andrea.bancolombia.userinterfaces;
-
-import net.thucydides.core.pages.PageObject;
-
-public class InicioBancolombia extends PageObject {
-
-}
--- Banistmo/src/main/java/co/com/andrea/bancolombia/utils/Constantes.java
+++
@@ -1,8 +0,0 @@
-package co.com.andrea.bancolombia.utils;
-
-public class Constantes {
-
-    public static final String CLIENTE = "usuario";
-    public static final String RESULTADO_CONVERSION= "9.54 %";
-
-}
